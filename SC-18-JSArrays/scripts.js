7c0cc7cfcb6f29c18f9210bdec2e53f16eba6db9
André Torres(Linux) <andre.tors448@gmail.com>
2025-04-25 22:29:53 -0300

    SC-18-265

---
+++ SC-18-JSArrays/scripts.js
@@ -0,0 +1,24 @@
+// 1 - arrays
+const lista = [1, 2, 3, 4, 5]
+
+console.log(lista)
+
+const items = ["Matheus", true, 2, 4.12, []]
+
+console.log(items)
+
+// 2 - Mais sobre arrays
+const arr = ["a", "b", "c", "d", "d"]
+console.log(arr[0])
+console.log(arr[1])
+
+// 3 - Propriedades
+const numbers = [5, 3, 4]
+
+console.log(numbers.length)
+
+console.log(numbers["length"])
+ 
+const myName = "Matheus"
+
+console.log(myName.length)

bec194bd754c3cf49192192bc7efea52ef7829c8
André Torres <150715327+andtors@users.noreply.github.com>
2025-05-09 10:39:45 -0300

    SC-35-Concluido

---
+++ SC-35-IntroReact/src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Navbar from './Navbar.jsx';
+import "bootstrap/dist/js/bootstrap.bundle.min";  
+import "bootswatch/dist/brite/bootstrap.min.css"
+
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  
+  <StrictMode>
+    <Navbar />
+    <App />
+  </StrictMode>
+)

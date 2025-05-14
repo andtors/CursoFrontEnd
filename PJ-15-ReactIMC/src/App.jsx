6e1dc4e462ac20f9eec691adc070c5e2e17c2ddf
André Torres <150715327+andtors@users.noreply.github.com>
2025-05-14 16:29:21 -0300

    PJ-15-Concluido

--- PJ-15-ReactIMC/src/App.jsx
+++ PJ-15-ReactIMC/src/App.jsx
@@ -1,34 +1,54 @@
+import { data } from "./data/data"
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+import ImcCalc from './components/ImcCalc'
+import ImcTable from "./components/ImcTable"
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [imc, setImc] = useState("")
+  const [info, setInfo] = useState("")
+  const [infoClass, setInfoClass] = useState("")
+
+  const calcImc = (e, height, weight) => {
+    e.preventDefault()
+
+    if(!weight || !height) return
+
+    const weightFloat = +weight.replace(",", ".")
+    const heightFloat = +height.replace(",", ".")
+
+    const imcResult = (weightFloat / (heightFloat * heightFloat)).toFixed(2)
+
+    setImc(imcResult)
+
+    data.forEach((item) => {
+      if(imcResult >= item.min && imcResult <= item.max ) {
+        setInfo(item.info)
+        setInfoClass(item.infoClass)
+      }
+    })
+
+    if(!info) return
+  }
+
+  const resetCalc = (e) => {
+    e.preventDefault()
+
+    setImc("")
+    setInfo("")
+    setInfoClass("")
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container'>
+      {!imc ? (
+        <ImcCalc calcImc={calcImc} />
+      ) : (
+        <ImcTable data={data} imc={imc} info={info} infoClass={infoClass} resetCalc={resetCalc}/>
+      )}
+    </div>
   )
 }
 

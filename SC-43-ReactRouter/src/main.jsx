ab6b60aeee0cc41ecdf453eba9f8036ebdd43345
André Torres <150715327+andtors@users.noreply.github.com>
2025-05-25 22:01:00 -0300

    SC-43-583

--- SC-43-ReactRouter/src/main.jsx
+++ SC-43-ReactRouter/src/main.jsx
@@ -4,13 +4,15 @@ import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
 
 import App from './App.jsx'
 import Contact from './routes/Contact.jsx'
+import ErrorPage from './routes/ErrorPage.jsx'
 
 import './index.css'
 
 const router = createBrowserRouter ([
   {
     path: "/",
-    element: <App />
+    element: <App />,
+    errorElement: <ErrorPage />
   },
   {
     path: "contact",

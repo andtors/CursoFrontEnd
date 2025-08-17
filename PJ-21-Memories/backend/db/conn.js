85429fe002628472672e091620e53e9de4e689b6
Andre <andre.tors448@gmail.com>
2025-08-16 21:13:00 -0300

    Models criado

---
+++ PJ-21-Memories/backend/db/conn.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+require("dotenv").config()
+
+mongoose.set("strictQuery", true)
+
+async function main() {
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j83thir.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`)
+
+    console.log("Conectado com sucesso!")
+}
+
+main().catch((err) => console.log(err))
+
+module.exports = main
---
+++ PJ-21-Memories/backend/models/Memory.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    }
+})
+
+const MemorySchema = new Schema({
+        title: {
+            type: String, 
+            require: true
+        },
+        src: {
+            type: String,
+            require: true
+
+        },
+        description: {
+            type: String, 
+            require: true
+        },
+        favorite:{
+            type: Boolean
+        },
+        comments: [commentSchema]
+    },
+    {timestamps: true}
+)

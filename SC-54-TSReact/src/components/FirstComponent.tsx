77f8eb65594e4d3b1922ff9ae44f8f0342acd227
Andre <andre.tors448@gmail.com>
2025-09-01 16:42:03 -0300

    SC-54-766

---
+++ SC-54-TSReact/src/components/FirstComponent.tsx
@@ -0,0 +1,13 @@
+import React, { type ReactElement } from 'react'
+
+type Props = {}
+
+const FirstComponent = (): ReactElement => {
+  return (
+    <div>
+        <h1>Meu primeiro componente</h1>
+    </div>
+  )
+}
+
+export default FirstComponent
